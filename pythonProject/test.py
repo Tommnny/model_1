b31251e53f1ad55598350e247abf8a876829b324
Tommnny <2015283660@qq.com>
2023-05-17 15:21:49 +0800

    1st

---
+++ pythonProject/test.py
@@ -0,0 +1,22 @@
+from time import *
+
+var = 564
+
+print("%d" % var)
+
+b = 1 == 1
+
+print(f"你是 : {b}")
+
+if 1 == 2:
+    if 5 == 7:
+        print({a})
+    else:
+        print({a})
+else:
+    print(var)
+
+sleep(10)
+
+print(55)
+
